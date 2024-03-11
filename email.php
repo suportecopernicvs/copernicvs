32a4cf28e3371504db1177feb92634be442d0a0c
Marcos Paulo <marcos.dev1996@gmail.com>
2024-03-10 21:22:42 -0300

    feat: This test, for pop-up e-mails sweetAlert

--- email.php
+++ email.php
@@ -8,59 +8,42 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 
 if (isset($_POST['enviar'])) {
-    $mail = new PHPMailer(true);
+  $mail = new PHPMailer(true);
 
-    try {
-        //Server settings
-        // $mail->SMTPDebug = SMTP::DEBUG_CONNECTION;                      
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'w.manage.2021@gmail.com';
-        $mail->Password   = 'emwfofiylpsyiile';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+  try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_CONNECTION;                      
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'w.manage.2021@gmail.com';
+    $mail->Password   = 'emwfofiylpsyiile';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        //Recipients
-        $mail->setFrom('suporte.copernicvs@gmail.com',);
-        $mail->addAddress('suporte.copernicvs@gmail.com');
-        $mail->addReplyTo('suporte.copernicvs@gmail.com',);
-        $mail->isHTML(true);
+    //Recipients
+    $mail->setFrom('suporte.copernicvs@gmail.com',);
+    $mail->addAddress('suporte.copernicvs@gmail.com');
+    $mail->addReplyTo('suporte.copernicvs@gmail.com',);
+    $mail->isHTML(true);
 
-        $mail->Subject = 'Cliente Corpernicvs';
+    $mail->Subject = 'Cliente Corpernicvs';
 
 
-        $body = "Mensagem de contato Cliente, <br>
+    $body = "Mensagem de contato Cliente, <br>
         Nome: " . $_POST['name'] . "<br> 
         E-mail: " . $_POST['email'] . "<br>
         Assunto: " . $_POST['subject'] . "<br>
         Menssagem:<br>" . $_POST['message'];
 
-        $mail->Body    = $body;
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Body    = $body;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mail->send();
-        echo "<script>Swal.fire({
-          'Email enviado com sucesso!',
-          'Por favor, aguarde nosso contato',
-          'success' ,
-        'showConfirmButton: false,
-        'timer: 1500'})</script>";
-        echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
-        header("Location: index.html");
-    } catch (Exception $e) {
-        echo "<script>Swal.fire(
-            'Erro!',
-            ' Algo Deu errado ao enviar e-mail',
-            'error' ,
-          'showConfirmButton: false,
-          'timer: 1500')</script>{$mail->ErrorInfo}";
-          echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
-        header("Location: index.html");
-        
-               
-       
-    }
+    $mail->send();
+    echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+    
+  } catch (Exception $e) {
+    echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+    
+  }
 }
-
-?>
--- js/main.js
+++ js/main.js
@@ -47,3 +47,23 @@ function smoothScrollTo(endX, endY, duration) {
     window.scroll(newX, newY);
   }, 100 / 60);
 }
+
+$(document).ready(function(){
+  $('#contact-form').on('submit',function(e) {  //Don't foget to change the id form
+  $.ajax({
+      url:'email.php', //===PHP file name====
+      data:$(this).serialize(),
+      type:'POST',
+      success:function(data){
+        console.log(data);
+        //Success Message == 'Title', 'Message body', Last one leave as it is
+      swal("¡Success!", "Enviado com sucesso, Por favor aguearde nosso contato!", "success");
+      },
+      error:function(data){
+        //Error Message == 'Title', 'Message body', Last one leave as it is
+      swal("Oops...", "Encotramos um problema para enviar seu e-mial, por favor no encaminhar uma mensagem no e-mial:suporte.copernicvs@gmail.com:(", "error");
+      }
+    });
+    e.preventDefault(); //This is to Avoid Page Refresh and Fire the Event "Click"
+  });
+});
